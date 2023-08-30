9781ec991e937fd4be06e1346bc80995cbe53122
freya waterbel <139182762+freyawaterbel@users.noreply.github.com>
2023-08-29 21:45:13 -0400

    planning for restaurant menu

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,31 +1,65 @@
 package org.launchcode;
+import java.util.Date;
 
 public class MenuItem {
-    private double price;
+    private String name;
+    private int price;
     private String description;
+    private boolean updated;
     private String category;
-    private boolean isNew;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
+    public String getName() {
+        return name;
     }
 
-    public void setPrice(double price) {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public boolean isUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(boolean updated) {
+        this.updated = updated;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setNew(boolean aNew) {
-        isNew = aNew;
+    public MenuItem(String name, int price, String description, String category, boolean updated) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.updated = updated;
+        this.category = category;
+    }
+
+
+
+    void displayItem () {
+        System.out.println(this.name + ", $" + this.price + ", " + this.description);
     }
 }
+
